0c00a88da162f1d298c54309f1a3f5d5509f6f3d
Job Finder User <user@example.com>
2026-02-19 19:24:19 +0500

    Update Zego credentials: new AppID 508512713 from client CallingApp project

--- KeyCenter.js
+++ KeyCenter.js
@@ -1,6 +1,6 @@
 const KeyCenter = {
-    appID: 744642506,
-    appSign: '7577ad42538a949fb5656cca1ff522c7815864db787e2c5dae28cf3d75e8b323',
-  };
-  
-  export default KeyCenter
+  appID: 508512713,
+  appSign: '783980854059f6f8a53512e8dd1b146b64f889153fd7a833ce3bf263ae527600',
+};
+
+export default KeyCenter
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,4 +1,4 @@
-import { Dimensions, Platform, PixelRatio } from 'react-native';
+wimport { Dimensions, Platform, PixelRatio } from 'react-native';
 
 // also in androidmanifest.xml
 // appdelegate.m
@@ -7,55 +7,55 @@ export const GOOGLE_API_KEY = "AIzaSyCr4gCy-Ec9U0Gk_twywPDBiHEHaMGDHC8"
 export const TOM_TOM_API_Key = "z2FW8yZb08nsGCzBTfGp48RmIimflD8t"
 export const RECAPTCHA_SITE_KEY = "6LfHkQUsAAAAAIhmGm4UdrDDbmvFoxXsOPNtGpXF";
 
-export const KeyCenter = { 
-	appID: 940046216, 
-	appSign: '6a969aba82a46e29ee8f8676252d31c37b121f64e66aafc301f9a1bac7d5d589', 
-}; 
+export const KeyCenter = {
+	appID: 508512713,
+	appSign: '783980854059f6f8a53512e8dd1b146b64f889153fd7a833ce3bf263ae527600',
+};
 
 export const AppColors = {
 	textBlack: '#171719',
 	textGrey: '#979797',
 	iconGrey: '#DCDCDC',
 	background: '#fff',
-	primaryColor:'#FF0202',
+	primaryColor: '#FF0202',
 	subHeadingColor: '#FE9DAE',
-	buttonColor:"#F40BDC",
-	buttonFontColor:"#fff",
-	containerBackground:'#DCDCDC',
+	buttonColor: "#F40BDC",
+	buttonFontColor: "#fff",
+	containerBackground: '#DCDCDC',
 	textInputBg: '#fff',
 	hyperlinks: '#FE9DAE',
-	borderColor:'#DCDCDC',
-	addcontatBtn:'#F4EFEF',
-	newPink:'#ED4FAA',
+	borderColor: '#DCDCDC',
+	addcontatBtn: '#F4EFEF',
+	newPink: '#ED4FAA',
 	uiblack: '#051225',
 	uigreybackground: '#D9D9D94D',
 	uitextborder: '#0512250d',
 	uitext: '#051225A6',
 	uibackgroundinactive: '#F1F5F9',
 	uitextinactive: '#CBD5E1',
-	uiBottomNavInactive:'#051225B2'
+	uiBottomNavInactive: '#051225B2'
 };
 
 
 export const SpaceConstants = {
-	spacing : 20
+	spacing: 20
 }
 
 const {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
+	width: SCREEN_WIDTH,
+	height: SCREEN_HEIGHT,
 } = Dimensions.get('window');
 
 // based on iphone 5s's scale
 const scale = SCREEN_WIDTH / 320;
 
 export function normalize(size) {
-    const newSize = size * scale
-    if (Platform.OS === 'ios') {
-        return Math.round(PixelRatio.roundToNearestPixel(newSize)) + 2
-    } else {
-        return Math.round(PixelRatio.roundToNearestPixel(newSize))
-    }
+	const newSize = size * scale
+	if (Platform.OS === 'ios') {
+		return Math.round(PixelRatio.roundToNearestPixel(newSize)) + 2
+	} else {
+		return Math.round(PixelRatio.roundToNearestPixel(newSize))
+	}
 }
 
 export const Font = {
@@ -65,16 +65,16 @@ export const Font = {
 	family_regular: 'Roboto-Regular',
 	family: 'Roboto-Regular',
 	headingSize: normalize(18),
-	subheadingSize : normalize(12),
-	textSize : normalize(11),
-	buttonSize:normalize(12),
+	subheadingSize: normalize(12),
+	textSize: normalize(11),
+	buttonSize: normalize(12),
 	headingBold: "600",
 	subheadingBold: "500"
 }
 
 export const scrollViewProperties = {
-    showsVerticalScrollIndicator:false,
-    bounces:false
+	showsVerticalScrollIndicator: false,
+	bounces: false
 }
 
 export const latitudeConstant = 38.900497
